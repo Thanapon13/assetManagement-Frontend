cda75c4ea7f39596862f920e5994665e9d6928da
kitar-kks <kroekkiat.kitar@gmail.com>
2022-12-26 14:30:00 +0700

    add dropdown_profile responsive

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import { DropdownProfile } from './DropdownProfile'
 import Sidebar from './Sidebar'
 
 export const Navbar = ({ children }) => {
   return (
     <div className="">
-      <div className="h-[56px] bg-text-green flex items-center ">
+      <div className="h-[56px] bg-text-green flex justify-between sm:justify-end items-center ">
         <div className="flex sm:hidden">
           <Sidebar />
         </div>
+        <DropdownProfile />
       </div>
       <main className="sm:hidden">{children}</main>
     </div>
