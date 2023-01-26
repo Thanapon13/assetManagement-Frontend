87cba7948bacdc21e73185774919c5a3460ef012
kitar-kks <kroekkiat.kitar@gmail.com>
2023-01-26 10:57:57 +0700

    feat/main fix submenu from merge

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,7 +5,7 @@ import User from '../components/auth/User'
 const Dashboard = () => {
   return (
     <div className="flex justify-center items-center h-[90vh] bg-gray-100 text-black text-5xl">
-      <User />
+      {/* <User /> */}
     </div>
   )
 }
